1097f41f66f629bd28691d46148db06c91712c68
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-11 15:24:46 +0200

    Create BankAccount class

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,27 @@
+import java.math.BigDecimal;
+
+public class BankAccount {
+    private int accountNumber;
+    private String accountName;
+    private BigDecimal balance = new BigDecimal("0.0");
+
+    public BankAccount(String accountName) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(BigDecimal amount) {
+
+    }
+
+    public void withdraw(BigDecimal amount) {
+
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void getAccountSummary() {
+
+    }
+}
