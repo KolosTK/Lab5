7b5db1bca7fadb76b84f9103ec527d0429b249d6
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-11 15:28:13 +0200

    Create Bank class

---
+++ src/main/java/Bank.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Bank {
+    private List<BankAccount> bankAccount;
+
+    public void createAccount(String accountName) {
+
+    }
+
+    public void findAccount(int accountNumber) {
+
+    }
+
+    public void transferMoney(int accountNumber, int toAccountNumber, BigDecimal amount) {
+    }
+
+}
