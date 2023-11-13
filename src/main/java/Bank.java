be3fe287274bdf49b8198d55a05cdc3bc4b89fe9
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-13 14:29:14 +0200

    Initialize createAccount method Add value to bankAccount List Initialize findAccount method

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,15 +1,27 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Bank {
-    private List<BankAccount> bankAccount;
+    private List<BankAccount> bankAccount = new ArrayList<BankAccount>();
 
-    public void createAccount(String accountName) {
-
+    public void createAccount(String accountName, BigDecimal initialDeposit) throws NegativeAmountException{
+        BankAccount bankAcc = new BankAccount(accountName,initialDeposit);
+        bankAccount.add(bankAcc);
+        if (initialDeposit.compareTo(BigDecimal.ZERO)<0)
+        {
+            throw new NegativeAmountException("You can't initialize negative amount of funds");
+        }
     }
 
-    public void findAccount(int accountNumber) {
-
+    public BankAccount findAccount(int accountNumber) throws AccountNotFoundException {
+        for (BankAccount account : bankAccount) {
+            if (account.getAccountNumber() == accountNumber) {
+                return account;
+            }
+        }
+        throw new AccountNotFoundException("Account not found");
     }
 
     public void transferMoney(int accountNumber, int toAccountNumber, BigDecimal amount) {
